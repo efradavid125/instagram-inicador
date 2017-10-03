c7e944210a75f24bdf8e845eeb2f809d739b07fa
efrain <efradavid125@gmail.com>
2017-10-03 12:02:49 -0400

    session initiator

---
+++ index.js
@@ -0,0 +1,16 @@
+var webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+
+ var user = require('./credentials');
+ console.log(user);
+
+driver.get('https://www.instagram.com/?hl=es');
+driver.findElement(By.className('_b93kq')).click();
+driver.findElement(By.name('username')).sendKeys(user.user);
+driver.findElement(By.name('password')).sendKeys(user.passw);
+driver.findElement(By.className('_qv64e _gexxb _4tgw8 _njrw0')).click();
